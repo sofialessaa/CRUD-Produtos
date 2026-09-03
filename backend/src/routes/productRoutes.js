26b01b954231a6095f1e566b9321892ab234d9c9
Sofia Lessa <sofialessa@outlook.com.br>
2026-09-03 09:08:20 -0300

    feat: configura rotas para produtos

---
+++ backend/src/routes/productRoutes.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import { createProduct, getAllProducts, updateProduct, deleteProduct } from '../controllers/productController.js';
+import { checkProductExists } from '../middleware/checkProductExists.js';
+
+const router = express.Router();
+
+router.post('/', createProduct);
+router.get('/', getAllProducts);
+router.put('/:id', checkProductExists, updateProduct);
+router.delete('/:id', checkProductExists, deleteProduct);
+
+export default router;
