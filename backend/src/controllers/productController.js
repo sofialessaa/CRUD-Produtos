ed09909e60d443692753fb2ae2b14c63c0e433d2
Sofia Lessa <sofialessa@outlook.com.br>
2026-09-03 09:07:47 -0300

    feat: implementação de controllers com logica do crud de produtos

---
+++ backend/src/controllers/productController.js
@@ -0,0 +1,53 @@
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+// POST - rota para criar um novo produto
+export const createProduct = async (req, res) => {
+  const {name, description, price, category, stock} = req.body;
+  try {
+    const product = await prisma.product.create({data: {name, description, price, category, stock}});
+    return res.status(201).json(product);
+  } catch (error) {
+    console.error('Erro ao criar produto:', error);
+    return res.status(500).json({error: 'Erro ao criar produto.'});
+  }
+};
+
+// GET - rota para listar todos os produtos
+export const getAllProducts = async (req, res) => {
+  try {
+    const getProducts = await prisma.product.findMany();
+    return res.status(200).json(getProducts);
+  } catch (error) {
+    console.error('Erro ao buscar produtos:', error);
+    return res.status(500).json({error: 'Erro ao buscar produtos.'});
+  }
+};
+
+// PUT - rota para atualizar um produto 
+export const updateProduct = async (req, res) => {
+  const {id} = req.params;
+  const {name, description, price, category, stock} = req.body;
+  try { 
+    const updatedProduct = await prisma.product.update({
+      where: { id: parseInt(id) },
+      data: { name, description, price, category, stock }
+    });
+    return res.status(200).json(updatedProduct);
+  } catch (error) {
+    console.error('Erro ao atualizar produto:', error);
+    return res.status(500).json({error: 'Erro ao atualizar produto.'});
+  }
+};
+
+// DELETE - rota para deletar um produto
+export const deleteProduct = async (req, res) => {
+  const {id} = req.params;
+  try {
+    const deletedProduct = await prisma.product.delete({where: {id: parseInt(id)}});
+    return res.status(200).json(deletedProduct);
+  } catch (error) {
+    console.error('Erro ao deletar produto:', error);
+    return res.status(500).json({error: 'Erro ao deletar produto.'});
+  }   
+};
