1438bbd8bdad0d06b0493bb1e5bf9db2e28c9347
Sofia Lessa <sofialessa@outlook.com.br>
2026-09-03 09:55:15 -0300

    feat: adiciona a rota usuários ecamada de proteção a rota produtos

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+import { auth } from './middleware/auth.js';
 
 const app = express();
 
 app.use(express.json());
-app.use('/products', productRoutes);
+app.use('/products', auth, productRoutes);
+app.use('/users', userRoutes);
 
 export default app;
