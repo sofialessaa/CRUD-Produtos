fb9f5ebf0385bfe9f6db31ab58a686fbede5e2c7
Sofia Lessa <sofialessa@outlook.com.br>
2026-09-04 10:56:35 -0300

    feat: atualiza componente principal da aplicação

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import Cadastro from "./pages/cadastro";
+import Login from "./pages/login";
+import Home from "./pages/pagesProdutos/home";
+import PrivateRoute from "./components/privateRoutes";
+import ProductForm from "./pages/pagesProdutos/form";
+import ProductList from "./pages/pagesProdutos/list";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <ToastContainer position="top-right" autoClose={3000} />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/cadastro" element={<Cadastro />} />
+        <Route path="/home" element={<PrivateRoute><Home /></PrivateRoute>} />
+        <Route path="/products" element={<PrivateRoute><ProductList /></PrivateRoute>} />
+        <Route path="/products/register" element={<PrivateRoute><ProductForm /></PrivateRoute>} />
+        <Route path="/products/edit/:id" element={<PrivateRoute><ProductForm /></PrivateRoute>}/>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
