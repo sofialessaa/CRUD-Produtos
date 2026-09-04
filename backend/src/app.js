f445dade54922fa1f694373fa438ff1f860afb23
Sofia Lessa <sofialessa@outlook.com.br>
2026-09-04 10:46:13 -0300

    feat: adiciona a dependência e configuração do cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,9 +2,11 @@ import express from 'express';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import { auth } from './middleware/auth.js';
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors()); // habilita o CORS para todas as rotas
 app.use(express.json());
 app.use('/products', auth, productRoutes);
 app.use('/users', userRoutes);
