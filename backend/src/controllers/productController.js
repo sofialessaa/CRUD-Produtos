22c26a681c65bfc31602e1a2f8eb1228782ff91c
Sofia Lessa <sofialessa@outlook.com.br>
2026-09-04 11:34:14 -0300

    feat: adiciona validação na busca por um produto especifico

--- backend/src/controllers/productController.js
+++ backend/src/controllers/productController.js
@@ -37,6 +37,10 @@ export const getProductById = async (req, res) => {
 
   try {
     const product = await prisma.product.findUnique({ where: { id: parseInt(id), userId: userId } });
+
+    if (!product) {
+      return res.status(404).json({ error: 'Produto não encontrado.' });
+    }
     return res.status(200).json(product);
   } catch (error) {
     console.error('Erro ao buscar produto:', error);
